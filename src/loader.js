d326f98c0f2792652251cce8e0310dc54104f4ba
zues <zues>
2022-05-23 17:32:58 -0400

    fix: loader height

--- src/loader.js
+++ src/loader.js
@@ -3,13 +3,13 @@ import ContentLoader from "react-content-loader"
 import { TailSpin} from  'react-loader-spinner'
 
 const MyLoader = (props) => (
-<div className="w-full flex justify-center items-center h-screen text-primary">
-<TailSpin
-    height="100"
-    width="100"
-    color='indigo'
-    ariaLabel='loading'
-  />
+  <div className="w-full flex justify-center items-center text-primary" style={{height: "650px"}}>
+    <TailSpin
+        height="100"
+        width="100"
+        color='indigo'
+        ariaLabel='loading'
+      />
 
 </div>
 )
